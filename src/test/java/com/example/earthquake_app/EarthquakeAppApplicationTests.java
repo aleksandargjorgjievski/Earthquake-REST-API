2d3667074233c221868ad748ae36c68602c5c531
Aleksandar Gjorgjievski <aleksandargjorgjievski0@gmail.com>
2026-04-14 19:21:28 +0200

    Initial commit

---
+++ src/test/java/com/example/earthquake_app/EarthquakeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.earthquake_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EarthquakeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

9ba6124a79fb33841847d1a7c7bd5d969f9438bf
Aleksandar Gjorgjievski <aleksandargjorgjievski0@gmail.com>
2026-04-15 12:50:03 +0200

    Saving earthquake data from geoJson to the local postgreSQL database. Added the ability to get the data from the database

--- src/main/java/com/example/earthquake_app/controller/EarthquakeController.java
+++ src/main/java/com/example/earthquake_app/controller/EarthquakeController.java
@@ -1,11 +1,24 @@
 package com.example.earthquake_app.controller;
 
+import com.example.earthquake_app.model.Earthquake;
+import com.example.earthquake_app.repository.EarthquakeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/api/earthquakes")
 public class EarthquakeController {
 
+    @Autowired
+    private EarthquakeRepository earthquakeRepository;
+
+    @GetMapping
+    public List<Earthquake> getAllEarthquakes() {
+        return earthquakeRepository.findAll();
+    }
 }
